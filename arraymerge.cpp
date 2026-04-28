64984537427628387c2f5a4d3bf157fb167b3f13
quaachoi <85405739+quaachoi@users.noreply.github.com>
2026-04-27 17:40:38 -0700

    submission

--- arraymerge.cpp
+++ arraymerge.cpp
@@ -7,6 +7,53 @@ void merge(int arr[], int left, int mid, int right)
     // Create temporary arrays
     // Copy data
     // Merge sorted halves back into arr
+    int size1 = mid - left + 1;
+    int size2 = right - mid;
+
+    int* leftArr = new int[size1];
+    int* rightArr = new int[size2];
+
+    for(int i = 0; i < size1; i++)
+    {
+        leftArr[i] = arr[left + i];
+    }
+    for(int i = 0; i < size2; i++)
+    {
+        rightArr[i] = arr[mid + 1 + i];
+    }
+
+    int i = 0;
+    int j = 0;
+    int k = left;
+
+    while(i < size1 && j < size2)
+    {
+        if(leftArr[i] <= rightArr[j])
+        {
+            arr[k] = leftArr[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = rightArr[j];
+            j++;
+        }
+        k++;
+    }
+
+    while(i < size1)
+    {
+        arr[k] = leftArr[i];
+        i++;
+        k++;
+    }
+
+    while(j < size2)
+    {
+        arr[k] = rightArr[j];
+        j++;
+        k++;
+    }
 }
 
 void mergeSort(int arr[], int left, int right)
@@ -18,6 +65,10 @@ void mergeSort(int arr[], int left, int right)
         // Recursively sort left half
         // Recursively sort right half
         // Merge both halves
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
     }
 }
 
